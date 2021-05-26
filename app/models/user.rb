b64f2d0ceca3cc1a4496e6b63d29019f7234ebb8
alvindcaesar <me@alvindcaesar.com>
2021-05-26 10:48:54 +0800

    Add model for room and user

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+
+  has_many :rooms
+  has_one_attached :avatar
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+end
