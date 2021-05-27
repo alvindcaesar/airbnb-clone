cb789b4300e79a6c031e00e6be478630c3e8dd2f
alvindcaesar <me@alvindcaesar.com>
2021-05-27 11:01:25 +0800

    Add association with Booking

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :rooms
   has_one_attached :avatar
+  has_many :bookings
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
